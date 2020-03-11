74d4911ea9a4c0e8e1e74842ec7436b040b418e1
Popescu-Iulian <popescu.iulian@protonmail.com>
2020-03-11 21:45:07 +0200

    display latest repos

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const USER_NAME = document.querySelector('input');
 const USERNAME_PROFILE = document.getElementById('username-profile');
-const ALERT_MSG = document.querySelector('.hidden');
 
-// Fetch user profile data
+// Fetch username profile data
 class GetGitHubProfile {
   constructor() {
     this.client_id = '3f4f67fc720ce998f7cc';
@@ -12,15 +11,19 @@ class GetGitHubProfile {
   async getUserProfile(username) {
     const PROFILE_RESPONSE = await fetch(`https://api.github.com/users/${username}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const REPOS_RESPONSE = await fetch(`https://api.github.com/users/${username}/repos?per_page=5&sort=asc&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const PROFILE_DATA = await PROFILE_RESPONSE.json();
+    const REPOS_DATA = await REPOS_RESPONSE.json();
 
     return {
-      profile: PROFILE_DATA
+      profile: PROFILE_DATA,
+      repos: REPOS_DATA
     }
   }
 }
 
-// Display fetched user profile data
+// Display fetched username profile data
 class DisplayGitHubProfile {
   constructor() {
     this.profile = USERNAME_PROFILE;
@@ -52,10 +55,36 @@ class DisplayGitHubProfile {
     `;
   }
 
-  // Show alerg message to display
+  displayUserRepos(repos) {
+    const USERNAME_REPOS = document.getElementById('username-repos');
+    let str = '';
+
+    repos.forEach((repo) => {
+      str += `
+        <div>
+          <div>
+            <div>
+              <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            </div>
+            <div>
+              <span>Stars: ${repo.stargazers_count}</span>
+              <span>Watchers: ${repo.watchers_count}</span>
+              <span>Forks: ${repo.forks_count}</span>
+            </div>
+          </div>
+        </div>
+      `;
+    });
+
+    USERNAME_REPOS.innerHTML = str;
+  }
+
+  // Display alerg message
   displayNotFound() {
     USERNAME_PROFILE.innerHTML = `
-      <h3 class="alert">Username Not Found!</h3>
+      <div class="alert">
+        <h3 class="alert-msg">Username Not Found!</h3>
+      </div>
     `;
   }
 }
@@ -68,15 +97,16 @@ const DISPLAY_PROFILE = new DisplayGitHubProfile;
 
 // Event that trigger search and display
 function searchUser() {
-  // Make HTTP call for user profile
+  // Make HTTP call for username profile
   GITHUB_USER.getUserProfile(USER_NAME.value)
     .then(data => {
       if (data.profile.message === 'Not Found') {
-        // Show alert
+        // Display alert
         DISPLAY_PROFILE.displayNotFound();
       } else {
         // Display profile
         DISPLAY_PROFILE.displayUserProfile(data.profile);
+        DISPLAY_PROFILE.displayUserRepos(data.repos);
       }
     })
 }
